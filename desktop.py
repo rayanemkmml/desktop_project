d7552bd6f5ffe25d6d0b033f719d40a4557cf26f
Rayane_algo <bounouhrayane4@gmail.com>
2025-05-16 18:29:11 +0100

    Add files via upload

---
+++ desktop.py
@@ -0,0 +1,5 @@
+# instr.py
+txt_title = "Health"
+txt_hello = "Welcome to the Health status detection program."
+txt_instructions = "Please follow the instructions to check your health status. This application allows you to use the Rufier test to make initial diagnosis of your health. '" \
+"Within 45 seconds, perform 30 squats. After that, you will be asked to measure your heart rate for the last 15seconds of the first minute of recovery. " 
